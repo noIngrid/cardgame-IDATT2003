3eca56f867ede4e2eb800a0e44f26cf6532efc2a
ingrop <ingop02@hotmail.com>
2025-03-21 17:30:50 +0100

    feat(deck): deal hand method

--- src/main/java/components/core/DeckOfCards.java
+++ src/main/java/components/core/DeckOfCards.java
@@ -1,13 +1,17 @@
 package components.core;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class DeckOfCards {
-  private final char[] suit = {'S', 'H', 'D', 'C'};
-  private ArrayList<PlayingCard> cards;
+
+  private final ArrayList<PlayingCard> cards;
+  Random random = new Random();
 
   public DeckOfCards() {
     cards = new ArrayList<>();
+    char[] suit = {'S', 'H', 'D', 'C'};
     for (char s : suit) {
       for (int i = 0; i < 13; i++) {
         cards.add(new PlayingCard(s, i + 1));
@@ -15,7 +19,7 @@ public class DeckOfCards {
     }
   }
 
-  public ArrayList<PlayingCard> getCards() {
+  public List<PlayingCard> getCards() {
     return cards;
   }
 
@@ -25,5 +29,20 @@ public class DeckOfCards {
 
   public PlayingCard getCardFromIndex(int index) {
     return cards.get(index);
+  }
+
+  public List<PlayingCard> dealHand(int n) {
+    ArrayList<PlayingCard> hand = new ArrayList<>();
+
+    for (int i = 0; i < n; i++) {
+      int cardIndex = random.nextInt(cards.size());
+      hand.add(cards.get(cardIndex));
+      cards.remove(cardIndex);
+    }
+    return hand;
+  }
+
+  public String getCardsAsString(PlayingCard card) {
+    return card.getAsString();
   }
 }
